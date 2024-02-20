7c0234500a3673bcec84425c13883ab8066892fa
VPagent <vpagent13@gmail.com>
2024-02-20 22:54:51 +0200

    build2

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <StorageProvider>
+      <StorageProvider baseName="VF-words/">
         <App />
       </StorageProvider>
     </BrowserRouter>
